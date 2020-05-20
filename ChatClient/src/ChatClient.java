55676e9936851cab98c09a3061405fff0e7ffb2b
Dinh Phu <ndp17hp@gmail.com>
2020-05-20 20:39:02 +0700

    File chay ngon 20:38 20-MAy

---
+++ ChatClient/src/ChatClient.java
@@ -0,0 +1,55 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class ChatClient {
+    private String serverName;
+    private int serverPort;
+    private InputStream inputStream;
+    private OutputStream outputStream;
+    private Socket socket;
+
+    public ChatClient(String serverName, int serverPort) {
+        this.serverName=serverName;
+        this.serverPort=serverPort;
+    }
+
+    public static void main(String[] args) {
+        ChatClient client= new ChatClient("localhost",9000);
+        if (client.connect()){
+            System.out.println("Connected successfull!");
+            client.send("hello server");
+
+        }else{
+
+        }
+    }
+
+    private boolean connect() {
+        try {
+            this.socket = new Socket(serverName,serverPort);
+            this.inputStream=socket.getInputStream();
+            this.outputStream=socket.getOutputStream();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+    public void send(String msg){
+        try {
+            this.outputStream.write(msg.getBytes());
+            this.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void close(){
+        try {
+            this.socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ ChatServer/src/ServerMain.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class ServerMain {
+    public static void main(String[] args) throws IOException {
+            int port= 9000;
+            ServerSocket serverSocket= new ServerSocket(port);
+
+            while (true){
+                System.out.println("wating for connecting");
+                Socket clientSocket=serverSocket.accept();
+                System.out.println("Connected Successfull with: "+clientSocket.getRemoteSocketAddress());
+                InputStream inputStream=clientSocket.getInputStream();
+                OutputStream outputStream=clientSocket.getOutputStream();
+                BufferedReader reader= new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line=reader.readLine())!=null ){
+                    System.out.println(line);
+                }
+
+            }
+
+    }
+}
